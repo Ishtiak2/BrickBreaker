f7e8994f9ef4c9d6fb8a62fad748e696cbca22d8
Ishtiak Rahman <ishtiakrahman13579@gmail.com>
2025-05-19 22:24:01 +0600

    update

--- src/game/GameFrame.java
+++ src/game/GameFrame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class GameFrame extends JFrame {
     public GameFrame() {
         setTitle("Brick Breaker");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit on close button
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit on the close button
         add(new GamePanel());
         pack(); // fit the size of the game panel
         setLocationRelativeTo(null); //centers the window
